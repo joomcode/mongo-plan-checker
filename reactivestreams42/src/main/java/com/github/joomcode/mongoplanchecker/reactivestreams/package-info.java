35c3610272ad7e252aa6fe19866483602e4eaaeb
Ivan Sopov <isopov@joom.com>
2021-04-05 16:52:00 +0300

    support mongo driver 4.2.*

---
+++ reactivestreams42/src/main/java/com/github/joomcode/mongoplanchecker/reactivestreams/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.github.joomcode.mongoplanchecker.reactivestreams;
+
+import com.github.joomcode.mongoplanchecker.core.NonNullApi;
---
+++ sync42/src/main/java/com/github/joomcode/mongoplanchecker/sync/PlanCheckerMongoClient.java
@@ -0,0 +1,119 @@
+package com.github.joomcode.mongoplanchecker.sync;
+
+import com.github.joomcode.mongoplanchecker.core.PlanChecker;
+import com.mongodb.ClientSessionOptions;
+import com.mongodb.client.*;
+import com.mongodb.connection.ClusterDescription;
+import java.util.List;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+public final class PlanCheckerMongoClient implements MongoClient {
+  private final MongoClient client;
+  private final PlanChecker checker;
+
+  public PlanCheckerMongoClient(MongoClient client, PlanChecker checker) {
+    this.client = client;
+    this.checker = checker;
+  }
+
+  @Override
+  public MongoDatabase getDatabase(String databaseName) {
+    return new PlanCheckerMongoDatabase(client.getDatabase(databaseName), checker);
+  }
+
+  @Override
+  public ClientSession startSession() {
+    return client.startSession();
+  }
+
+  @Override
+  public ClientSession startSession(ClientSessionOptions options) {
+    return client.startSession(options);
+  }
+
+  @Override
+  public void close() {
+    client.close();
+  }
+
+  @Override
+  public MongoIterable<String> listDatabaseNames() {
+    return client.listDatabaseNames();
+  }
+
+  @Override
+  public MongoIterable<String> listDatabaseNames(ClientSession clientSession) {
+    return client.listDatabaseNames(clientSession);
+  }
+
+  @Override
+  public ListDatabasesIterable<Document> listDatabases() {
+    return client.listDatabases();
+  }
+
+  @Override
+  public ListDatabasesIterable<Document> listDatabases(ClientSession clientSession) {
+    return client.listDatabases(clientSession);
+  }
+
+  @Override
+  public <TResult> ListDatabasesIterable<TResult> listDatabases(Class<TResult> tResultClass) {
+    return client.listDatabases(tResultClass);
+  }
+
+  @Override
+  public <TResult> ListDatabasesIterable<TResult> listDatabases(
+      ClientSession clientSession, Class<TResult> tResultClass) {
+    return client.listDatabases(clientSession, tResultClass);
+  }
+
+  @Override
+  public ChangeStreamIterable<Document> watch() {
+    return client.watch();
+  }
+
+  @Override
+  public <TResult> ChangeStreamIterable<TResult> watch(Class<TResult> tResultClass) {
+    return client.watch(tResultClass);
+  }
+
+  @Override
+  public ChangeStreamIterable<Document> watch(List<? extends Bson> pipeline) {
+    return client.watch(pipeline);
+  }
+
+  @Override
+  public <TResult> ChangeStreamIterable<TResult> watch(
+      List<? extends Bson> pipeline, Class<TResult> tResultClass) {
+    return client.watch(pipeline, tResultClass);
+  }
+
+  @Override
+  public ChangeStreamIterable<Document> watch(ClientSession clientSession) {
+    return client.watch(clientSession);
+  }
+
+  @Override
+  public <TResult> ChangeStreamIterable<TResult> watch(
+      ClientSession clientSession, Class<TResult> tResultClass) {
+    return client.watch(clientSession, tResultClass);
+  }
+
+  @Override
+  public ChangeStreamIterable<Document> watch(
+      ClientSession clientSession, List<? extends Bson> pipeline) {
+    return client.watch(clientSession, pipeline);
+  }
+
+  @Override
+  public <TResult> ChangeStreamIterable<TResult> watch(
+      ClientSession clientSession, List<? extends Bson> pipeline, Class<TResult> tResultClass) {
+    return client.watch(clientSession, pipeline, tResultClass);
+  }
+
+  @Override
+  public ClusterDescription getClusterDescription() {
+    return client.getClusterDescription();
+  }
+}
