5736c7ce1fce313b5d330140b855b1c28dbfbfc9
Ivan Sopov <isopov@joom.com>
2021-05-31 13:43:47 +0300

    support spring-boot 2.5 and bump dep versions

---
+++ samples/spring-boot25-reactive/src/main/java/com/github/joomcode/mongoplanchecker/reactivestreams/sample/PlayerRepository.java
@@ -0,0 +1,11 @@
+package com.github.joomcode.mongoplanchecker.reactivestreams.sample;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
+
+public interface PlayerRepository extends ReactiveMongoRepository<GameOfThronesPlayer, ObjectId> {
+  Mono<Long> countByName(String name);
+
+  Mono<Long> countByHouse(String house);
+}
