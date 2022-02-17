f5eb3bc15bc889b7f199a1239eee9a11c6fc0cf5
Ivan Sopov <isopov@joom.com>
2022-02-17 18:59:26 +0300

    fix compatibility with mongo driver 4.5.0

--- core/src/main/java/com/github/joomcode/mongoplanchecker/core/Util.java
+++ core/src/main/java/com/github/joomcode/mongoplanchecker/core/Util.java
@@ -2,6 +2,7 @@ package com.github.joomcode.mongoplanchecker.core;
 
 import org.bson.codecs.BsonValueCodecProvider;
 import org.bson.codecs.DocumentCodecProvider;
+import org.bson.codecs.IterableCodecProvider;
 import org.bson.codecs.ValueCodecProvider;
 import org.bson.codecs.configuration.CodecRegistries;
 import org.bson.codecs.configuration.CodecRegistry;
@@ -9,5 +10,8 @@ import org.bson.codecs.configuration.CodecRegistry;
 class Util {
   static final CodecRegistry CODEC_REGISTRY =
       CodecRegistries.fromProviders(
-          new DocumentCodecProvider(), new BsonValueCodecProvider(), new ValueCodecProvider());
+          new DocumentCodecProvider(),
+          new BsonValueCodecProvider(),
+          new ValueCodecProvider(),
+          new IterableCodecProvider());
 }
